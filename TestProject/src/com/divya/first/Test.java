3b982bd85d9f43093413117666dbbf1d2df415fe
Divya <divyakaapa@gmail.com>
2018-02-05 15:55:07 -0600

    Adding first project files

---
+++ TestProject/src/com/divya/first/Test.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.divya.first;
+
+/**
+ * @author divya
+ *
+ */
+public class Test {
+	int Sum=  10+20;
+	//System.out.Println(Sum);
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ Test test=new Test();
+  
+  System.out.print(test.Sum);
+ 	} 
+
+
+}
